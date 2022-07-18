548ada119a37e0ea90d164caaa0ca4e17063fd54
Owen L <owenluu111@gmail.com>
2022-07-17 21:57:55 -0500

    Initial Commit

---
+++ netflixClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netflixClone
+//
+//  Created by Owen Luu on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netflixClone/netflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixCloneApp.swift
+//  netflixClone
+//
+//  Created by Owen Luu on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct netflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
