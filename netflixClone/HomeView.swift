bc867c8d674583807db8c78b98e36f706aecae10
Owen L <owenluu111@gmail.com>
2022-07-17 22:06:17 -0500

    Added a HomeView swift file. Then went to the App view page to call HomeView instead of ContentView

---
+++ netflixClone/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  netflixClone
+//
+//  Created by Owen Luu on 7/17/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Welcome to Netflix!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- netflixClone/netflixCloneApp.swift
+++ netflixClone/netflixCloneApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct netflixCloneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
