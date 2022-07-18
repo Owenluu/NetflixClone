4421433d9ced08f083a423a272084d062aa24b8a
Owen L <owenluu111@gmail.com>
2022-07-17 22:22:24 -0500

    added a movie model file and a standard home movie swiftUI file to begin passing movies into the screen

---
+++ netflixClone/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  netflixClone
+//
+//  Created by Owen Luu on 7/17/22.
+//
+
+import Foundation
+
+struct Movie {
+    var id: String
+    var name: String
+    var thumbnailURL: URL
+}
---
+++ netflixClone/StandardHomeMovie.swift
@@ -0,0 +1,22 @@
+//
+//  StandardHomeMovie.swift
+//  netflixClone
+//
+//  Created by Owen Luu on 7/17/22.
+//
+
+import SwiftUI
+
+struct StandardHomeMovie: View {
+    var movie: Movie
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StandardHomeMovie_Previews: PreviewProvider {
+    static var previews: some View {
+        StandardHomeMovie(movie: Movie(id: <#T##String#>, name: <#T##String#>, thumbnailURL: <#T##URL#>))
+    }
+}
