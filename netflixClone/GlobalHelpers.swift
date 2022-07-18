5fdc732d554b5b8b21b2e360aef2e7bded10551b
Owen L <owenluu111@gmail.com>
2022-07-17 22:28:08 -0500

    added a global helpers file and literally added an example movie for testing

---
+++ netflixClone/GlobalHelpers.swift
@@ -0,0 +1,10 @@
+//
+//  GlobalHelpers.swift
+//  netflixClone
+//
+//  Created by Owen Luu on 7/17/22.
+//
+
+import Foundation
+
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "Dark", thumbnailURL: URL(string: "https://picsum.photos/200/300")!)
