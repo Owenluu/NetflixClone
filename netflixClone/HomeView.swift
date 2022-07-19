be40f9e9083a02890a0cc8f0df36efc4c89d8e36
Owen L <owenluu111@gmail.com>
2022-07-18 20:48:06 -0500

    added VStack and inside the Stack, an HStack with a spacer to add the text to the left and then a scroll View to allow horizontal movement of the placeholder text which we will change out next.

--- netflixClone/HomeView.swift
+++ netflixClone/HomeView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
+    var vm: HomeVM = HomeVM()
+    
     var body: some View {
         ZStack{
             Color.black.edgesIgnoringSafeArea(.all)
             
-            ForEach(data: _) { category in
-                Text(category)
+            ForEach(vm.allCategories, id: \.self) { category in
+                VStack {
+                    HStack{
+                    Text(category)
+    //                    .foregroundColor(.blue)
+                        Spacer()
+                    }
+                    ScrollView(.horizontal, showsIndicators: false, content: {
+                        Text("placeholder")})
+                }
             }
         }
+        .foregroundColor(.white)
     }
 }
 
