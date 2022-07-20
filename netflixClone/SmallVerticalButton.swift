449f4c188f6211d5800fa6f44b83665f95c0eef8
Owen L <owenluu111@gmail.com>
2022-07-19 23:58:30 -0500

    created a new swiftUI file called small swift preview button

---
+++ netflixClone/SmallVerticalButton.swift
@@ -0,0 +1,23 @@
+//
+//  SmallVerticalButton.swift
+//  netflixClone
+//
+//  Created by Owen Luu on 7/19/22.
+//
+
+import SwiftUI
+
+struct SmallVerticalButton: View {
+    var text: String
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SmallVerticalButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallVerticalButton()
+    }
+}
